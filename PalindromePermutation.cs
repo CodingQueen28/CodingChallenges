dab57534675078fb4aa6c0ed2cd2ce593099e6ef
Maedeh Mousavi <maedeh.mousavi@finshark.io>
2021-05-01 16:08:55 +0200

    Add project files.

---
+++ PalindromePermutation.cs
@@ -0,0 +1,28 @@
+﻿namespace CodingChallenges
+{
+    public class PalindromePermutation
+    {
+        //Check if string's permutation is palindrome!
+        public bool checkPalindromePermutation(string s)
+        {
+            int[] char_counts = new int[128]; //upper and lower case ASCII characters
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                char_counts[s[i]]++;
+            }
+
+            int count = 0;
+
+            for (int i = 0; i < 128; i++)
+            {
+                if (char_counts[i] % 2 == 1)
+                {
+                    count++;
+                }
+            }
+            if (count <= 1) return true; //if there is one odd character in strig which doesn't match with any other character it is palindrome
+            return false;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CodingChallenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PalindromePermutation palindromePermutation = new PalindromePermutation();
+
+            //permutation means rearrangement of string characters aab => aba which is palindrome!
+            Console.WriteLine(palindromePermutation.checkPalindromePermutation("aab")); //returns true 
+
+            Console.WriteLine(palindromePermutation.checkPalindromePermutation("code"));//returns false
+        }
+    }
+}
